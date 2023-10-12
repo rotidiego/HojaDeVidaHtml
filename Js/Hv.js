cf89eb17ac607e28a0ac0d04a3510594aa749212
rotidiego <diegorodriguez1101@gmail.com>
2023-10-12 16:44:22 -0500

    a

--- Js/Hv.js
+++ Js/Hv.js
@@ -3,52 +3,7 @@ let Repositorios;
 let Estudios;
 $(document).ready(function () {
     console.log("ready")
-    Repositorios = [
-        {
-            "Nombre": "Frecuencia y Entropia de Videos",
-            "Descrpicion": "",
-            "Url": "",
-            "Img": "https://cdn-icons-png.flaticon.com/512/20/20513.png"
-        },
-        {
-            "Nombre": "Calculadora de tasas de intereses",
-            "Descrpicion": "",
-            "Url": "",
-            "Img": "https://cdn-icons-png.flaticon.com/512/3625/3625050.png"
-        },
-        {
-            "Nombre": "Netflix v1",
-            "Descrpicion": "",
-            "Url": "",
-            "Img": "https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456"
-        },
-        {
-            "Nombre": "Netflix v2",
-            "Descrpicion": "",
-            "Url": "",
-            "Img": "https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456"
-        }
-    ];
-    Experiencia = [
-        {
-            "Empresa": "Intergrupo",
-            "Cargo": "Aprendiz",
-            "FechaInicio": "29/11/2022",
-            "FechaFin": "28/05/2022",
-            "Img": "https://www.revistacloudcomputing.com/wp-content/uploads/2020/11/softwareone-intergrupo.jpg",
-            "Responsabilidades": ""
-        },
-        {
-            "Empresa": "SoftwareOne Intergrupo",
-            "Cargo": "Ingeniero de Desarrollo",
-            "FechaInicio": "06/06/2022",
-            "FechaFin": "Actualidad",
-            "Img": ""
-        }
-    ];
-    CargarRepos();
-    CargarExperiencia();
-    //   ObtenerData();
+    ObtenerData();
     function ObtenerData() {
         fetch('Data/Data.json', {
             mode: 'no-cors',
@@ -70,6 +25,9 @@ $(document).ready(function () {
                 Experiencia = data['Experiencia'];
                 Repositorios = data['Repositorios'];
                 Estudios = data['Estudios'];
+
+                CargarRepos();
+                CargarExperiencia();
             })
             .catch(error => console.error('Error:', error));
 
@@ -94,19 +52,19 @@ $(document).ready(function () {
     }
     function CargarExperiencia() {
         let htmlExp = '';
-        let htmlList = '<ul class="list-group list-group-flush">'
+        let htmlList = '<div class="card"><ul class="list-group list-group-flush">'
         let aux = '';
         for (let i = 0; i < Experiencia['length']; i++) {
             aux = i == 0 ? 'style="display: none;' : '';
             htmlList += '<li class="list-group-item" onclick="CambiarExperiencia(' + i + ')" style="cursor:pointer;">' + Experiencia[i]['Empresa'] + ' - ' + Experiencia[i]['FechaInicio'] + '</li>';
-            htmlExp += '<div class="Experiencia" id="Exp' + i + '" '+aux+'>'
+            htmlExp += '<div class="Experiencia" id="Exp' + i + '" ' + aux + '>'
             htmlExp += '<div class="row">'
             htmlExp += '<div class="col-lg-5">'
             htmlExp += '<img class="img-fluid"'
             htmlExp += 'src="' + Experiencia[i]['Img'] + '"'
             htmlExp += 'alt="Foto ' + Experiencia[i]['Empresa'] + '">'
             htmlExp += '</div>'
-            htmlExp += '<div class="col-6">'
+            htmlExp += '<div class="col">'
             htmlExp += '<div class="ExperienciaInfo">'
             htmlExp += '<h2>' + Experiencia[i]['Empresa'] + '</h2>'
             htmlExp += '<h5><strong>' + Experiencia[i]['Cargo'] + '</strong></h5>'
@@ -118,7 +76,7 @@ $(document).ready(function () {
             htmlExp += '</div>'
             htmlExp += '</div>'
         }
-        htmlList += '</ul>'
+        htmlList += '</ul></div>'
         $('#ExperienciaLista').html(htmlList);
         $('#ExperienciaDetalle').html(htmlExp);
     }
